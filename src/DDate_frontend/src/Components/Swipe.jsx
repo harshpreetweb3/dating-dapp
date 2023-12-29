62b79057e2d2d92129df1a9815e9336420cb817a
mohit <itsmohit30@gmail.com>
2023-12-29 13:46:35 +0530

    swipe responsive

--- src/DDate_frontend/src/Components/Swipe.jsx
+++ src/DDate_frontend/src/Components/Swipe.jsx
@@ -870,11 +870,19 @@ function Swipe() {
         <div className="sm:ml-64">
           <div
             className="container flex justify-center mr-1"
-            style={{ paddingRight: "23rem" }}
           >
-            <div className="max-w-xs md:max-w-md lg:max-w-lg xl:max-w-xl bg-white rounded-xl shadow-2xl shadow-slate-100 h-screen overflow-hidden">
+            <div className="max-w-xs md:max-w-md lg:max-w-lg xl:max-w-xl bg-white rounded-xl shadow-2xl shadow-slate-100 h-screen overflow-hidden
+            relative w-[380px] my-10">
               <div>
-                {db.map((character, index) => (
+             { db.length === 0 ? (
+          <div className="flex justify-center">
+            <div className="max-w-xs md:max-w-md lg:max-w-lg xl:max-w-xl bg-white  h-screen ">
+              <div className="h-screen">
+                <Loader />
+              </div>
+            </div>
+          </div>
+      ) :db.map((character, index) => (
                   <TinderCard
                     ref={childRefs[index]}
                     className="swipe"
@@ -891,23 +899,20 @@ function Swipe() {
                       <img
                         alt="img"
                         src={character.images[0]}
-                        className="h-full object-cover rounded-xl relative "
-                        style={{ height: "83vh", top: "-83px" }}
+                        className="h-full object-cover rounded-xl relative w-full -top-[5.5rem]"
+                        style={{ height: "83vh"}}
                       />
                     </div>
                     <div
-                      className="bg-black h-[256px] w-full z-10 "
+                      className="bg-black h-[200px] w-full z-10 rounded-2xl fixed -bottom-7"
                       style={{
                         background:
                           "linear-gradient(to top, rgb(0, 0, 0) 64%, rgba(255, 255, 255, 0) 100%)",
-                        position: "fixed",
-                        bottom: "-28px",
                       }}
                     ></div>
 
                     <div
                       className="pl-4 bottom-16 absolute z-21"
-                      // style={{ marginBottom: "-7px", lineHeight: "4px" }}
                     >
                       <h2 className="text-4xl font-bold text-white z-30 relative">
                         {character.name}
@@ -915,9 +920,9 @@ function Swipe() {
                       <p className="text-lg text-gray-700 font-bold z-30 relative">
                         {character.location}
                       </p>
-                      {console.log(character.id)}
+                      {/* {console.log(character.id)}
                       {console.log(character.location)}
-                      {console.log(character.images[0])}
+                      {console.log(character.images[0])} */}
                       <p className="mt-2 z-30 relative font-bold text-white mb-6">
                         {character.introduction}
                       </p>
@@ -935,25 +940,26 @@ function Swipe() {
                   </TinderCard>
                 ))}{" "}
                 <div
-                  className="px-0 flex absolute gap-4 pl-4 pt-2 py-6 m-0 z-40"
-                  style={{ bottom: "-43px" }}
+                  className="px-0 flex absolute gap-4 pl-4 pt-2 py-6 m-0 z-40 bottom-0"
                 >
                   <button
                     className="rounded-full  h-12 w-12 bg-transparent shadow-md text-3xl border border-pink-700 font-bold text-gray-800"
                     onClick={() => swipe("left")}
+                    disabled={db.length === 0}
                   >
                     <FontAwesomeIcon
                       icon={faClose}
-                      style={{ color: "#fd5068" }}
+                      style={{ color: db.length === 0 ? '#b2b2b2':"#fd5068" }}
                     />
                   </button>
                   <button
                     className="rounded-full  h-12 w-12 bg-transparent shadow-md text-3xl border border-green-700 font-bold text-gray-800"
                     onClick={() => swipe("right")}
+                    disabled={db.length === 0}
                   >
                     <FontAwesomeIcon
                       icon={faHeart}
-                      style={{ color: "#1be4a1" }}
+                      style={{ color:  db.length === 0 ?'#b2b2b2':"#1be4a1" }}
                     />
                   </button>
                 </div>
